d4b6f66526bf9b62f1260c544e15af4d333c067e
dnfvnsxn <dnfvnsxn@naver.com>
2019-02-04 01:33:20 +0900

    Updates

---
+++ precache-manifest.08e7660e0cb5b1c09bf19336a3c241c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "c8e7f906be904ac6a122",
+    "url": "/movie_app/static/js/main.c8e7f906.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "c8e7f906be904ac6a122",
+    "url": "/movie_app/static/css/main.83a2f6de.chunk.css"
+  },
+  {
+    "revision": "b7f5841d9d6cb22a4732249c03cf5261",
+    "url": "/movie_app/index.html"
+  }
+];
